73a0318c3bc7c75d032a5d318c2fa3738db6bf18
feng <13653824852@163.com>
2017-09-25 19:41:53 +0800

    修改javaFirst

--- project-test/src/com/subl/first/JavaFirst.java
+++ project-test/src/com/subl/first/JavaFirst.java
@@ -7,5 +7,10 @@ public class JavaFirst {
 	@Test
 	public void testHelloWrold() {
 		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
 	}
 }
